f01bf301907e617fc77a49cd37accfaec5e02ebb
Gibson Nakokonya <Gibson.Nakokonya@equitybank.co.ke>
2026-07-06 09:51:55 +0300

    fix: app.routes

--- projects/shell/src/app/app.routes.ts
+++ projects/shell/src/app/app.routes.ts
@@ -6,10 +6,7 @@ export const routes: Routes = [
   {
     path: 'customer360',
     loadComponent: () =>
-      loadRemoteModule('customer360', './Component').then(m => {
-        alert("Hellooo")
-        return m.AppComponent
-      }),
+      loadRemoteModule('customer360', './Component').then(m => m.AppComponent),
   },
 ];
 

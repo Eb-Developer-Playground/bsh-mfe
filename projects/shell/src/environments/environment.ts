d90811f6cf9d4d3d84e1b13b3b5bcbb01f6eeefe
Owen Adirah <owenadira@gmail.com>
2026-07-10 17:32:51 +0300

    Environments and assets
    Ultraworked with [Sisyphus](https://github.com/code-yeongyu/oh-my-openagent)
    
    Co-authored-by: Sisyphus <clio-agent@sisyphuslabs.ai>

---
+++ projects/shell/src/environments/environment.ts
@@ -0,0 +1,84 @@
+import {
+  Feature,
+  FeatureFlagsConfig,
+} from '@app/core/feature-flags/feature-flags.model';
+
+export const environment = {
+  production: false,
+  ngrxLogs: true,
+  enableRouterTracing: true,
+  apiUrl: 'https://api-uat.equitygroupholdings.com',
+  appUrl: 'https://servicehub-customer-360-uat.equitygroupholdings.com',
+  adminPortalUrl: 'https://servicehub-admin-portal-uat.equitygroupholdings.com',
+  customerOnboardingUrl:
+    'https://servicehub-customer-onboarding-uat.equitygroupholdings.com',
+  swiftUrl: 'https://servicehub-swift-uat.equitygroupholdings.com',
+  cardsPortalUrl: 'https://servicehub-cards-uat.equitygroupholdings.com',
+  bioExtPage: 'https://10.1.9.54:58379/compas-web',
+  secugenApi: 'https://localhost:8443',
+  secugenLicenseUAT:
+    '5aQfTiuSoBOfhoiN/RuOiwFWba1E9FBBNoHz4mx5b1dYNb9/OwvSiGXPfjj++FvFU1/bdqyJQpzinDuHgMZvSg==',
+  encryptionKey: 'S3cr3etKey$123',
+  PAGE_SIZE: 20,
+  useMockServiceWorker: false,
+  showAllMenuItems: true,
+  featureFlags: {
+    globalDefaults: {
+      [Feature.StandingOrder]: true,
+      [Feature.ChangeOfSignatories]: true,
+      [Feature.KnownAgent]: true,
+      [Feature.ChangeofMandate]: true,
+      [Feature.ChangeOfSignature]: true,
+    },
+    bySubsidiary: {
+      KE: {[Feature.UpdateCifPopUp]: false},
+      UG: {
+        [Feature.StandingOrder]: false,
+        [Feature.KnownAgent]: false,
+        [Feature.ChangeOfSignatories]: false,
+        [Feature.ChangeOfSignature]: false,
+      },
+      TZ: {
+        [Feature.StandingOrder]: false,
+        [Feature.KnownAgent]: false,
+        [Feature.ChangeOfSignatories]: false,
+        [Feature.ChangeofMandate]: false,
+        [Feature.ChangeOfSignature]: false,
+      },
+      CD: {},
+      RW: {
+        [Feature.StandingOrder]: false,
+        [Feature.KnownAgent]: false,
+        [Feature.ChangeOfSignatories]: false,
+        [Feature.ChangeOfSignature]: false,
+      },
+      SS: {
+        [Feature.StandingOrder]: false,
+        [Feature.KnownAgent]: false,
+        [Feature.ChangeOfSignatories]: false,
+        [Feature.ChangeOfSignature]: false,
+      },
+    },
+    actionFlowNames: {
+      KE: {
+        [Feature.StandingOrder]: 'StandingOrder',
+        [Feature.KnownAgent]: 'KnownAgentFlow',
+        [Feature.ChangeOfSignatories]: 'ChangeOfSignatories',
+        [Feature.ChangeofMandate]: 'ChangeOfMandate',
+        [Feature.ChangeOfSignature]: 'ChangeOfSignature',
+      },
+      UG: { [Feature.ChangeofMandate]: 'ChangeOfMandate' },
+      TZ: {},
+      CD: {
+        [Feature.StandingOrder]: 'StandingOrder',
+        [Feature.ChangeofMandate]: 'ChangeOfMandate',
+        [Feature.ChangeOfSignature]: 'ChangeOfSignature',
+        [Feature.ChangeOfSignatories]: 'ChangeOfSignatories',
+      },
+      RW: { [Feature.ChangeofMandate]: 'ChangeOfMandate' },
+      SS: { [Feature.ChangeofMandate]: 'ChangeOfMandate' },
+    },
+  } satisfies FeatureFlagsConfig,
+  // datadogRumId: '49feee3d-e9a9-48df-9ec8-8cc7dc89d15e',
+  // datadogRumToken: 'pub5be53aef7ad705e72bc7182eaa6552a4',
+};

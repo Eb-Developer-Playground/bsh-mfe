3700ea10e36dbe969383749acd2eb8d7baf773fb
Owen Adirah <owenadira@gmail.com>
2026-07-10 17:32:51 +0300

    Environments and assets

---
+++ projects/shell/src/environments/environment.pilot.ts
@@ -0,0 +1,22 @@
+export const environment = {
+  production: true,
+  ngrxLogs: false,
+  enableRouterTracing: false,
+  apiUrl: 'https://api.equitygroupholdings.com/pilot',
+  serverUrl: 'https://api-omnichannel-preprod.azure-api.net/pilot',
+  appUrl: 'https://servicehub-customer-360-pilot.equitygroupholdings.com',
+  adminPortalUrl:
+    'https://servicehub-admin-portal-pilot.equitygroupholdings.com',
+  customerOnboardingUrl:
+    'https://servicehub-customer-onboarding-pilot.equitygroupholdings.com',
+  swiftUrl: 'https://servicehub-swift-pilot.equitygroupholdings.com',
+  cardsPortalUrl: 'https://servicehub-cards-pilot.equitygroupholdings.com',
+  secugenLicenseProd:
+    '5aQfTiuSoBOfhoiN/RuOiyRbbBy+DjMe4Ws1ote3oQwwOrflykkzvNDyOxH1fTWd2NlHJFjHzj2W4Z0GldyJJuXPK97o2ikC9jATPaZtDkQ=',
+  bioExtPage: 'https://10.1.9.54:58379/compas-web',
+  secugenApi: 'https://localhost:8443',
+  clientId: '44FEC6A5E3F5454',
+  encryptionKey: 'S3cr3etKey$123',
+  PAGE_SIZE: 20,
+  showAllMenuItems: true,
+};
---
+++ projects/shell/src/environments/environment.prod.ts
@@ -0,0 +1,23 @@
+export const environment = {
+  production: true,
+  ngrxLogs: false,
+  enableRouterTracing: false,
+  apiUrl: 'https://api.equitygroupholdings.com',
+  appUrl: 'https://servicehub-customer-360-prod.equitygroupholdings.com',
+  adminPortalUrl:
+    'https://servicehub-admin-portal-prod.equitygroupholdings.com',
+  customerOnboardingUrl:
+    'https://servicehub-customer-onboarding-prod.equitygroupholdings.com',
+  swiftUrl: 'https://servicehub-swift-prod.equitygroupholdings.com',
+  cardsPortalUrl: 'https://servicehub-cards-prod.equitygroupholdings.com',
+  secugenLicenseProd:
+    '5aQfTiuSoBOfhoiN/RuOi+HyJkG4MBX5VxeKx3IFOKrWeHudn75T1HTWh5kvg/NnT/r6681lNyTqfeY0auLc0w==',
+  bioExtPage: 'https://10.1.9.54:58379/compas-web',
+  secugenApi: 'https://localhost:8443',
+  clientId: '44FEC6A5E3F5454',
+  PAGE_SIZE: 20,
+  encryptionKey: 'S3cr3etKey$123',
+  showAllMenuItems: false,
+  datadogRumId: '',
+  datadogRumToken: '',
+};
